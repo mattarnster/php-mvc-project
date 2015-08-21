763d0806d97edaa3557ea12ab056d8aeff982ebb
Matt Arnold <matthew.arnold@m-w.co.uk>
2015-08-21 18:36:30 +0100

    Removing redundant Use statement

--- App/Models/Post.php
+++ App/Models/Post.php
@@ -2,8 +2,6 @@
 
 namespace App\Models;
 
-use \App\Config\ConfigReader;
-
 class Post extends Model {
   private $table = "post";
   private $fields = array('id', 'post_title', 'post_text', 'post_created_at');
